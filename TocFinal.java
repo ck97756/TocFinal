340626688f7917b68b1e225c3d306c07ad6c9324
Jalu <ck97756@yahoo.com.tw>
2014-07-04 08:07:42 +0800

    Update TocFinal.java

--- TocFinal.java
+++ TocFinal.java
@@ -78,15 +78,15 @@ public class Proj {
 		if(l==2){
 			ArrayList<ArrayList<Integer[][]>> l2;
 			l2 =new ArrayList<ArrayList<Integer[][]>>();
-			for(int a=0;a<5;a++){
+			for(int a=0;a<keys.length;a++){
 				ArrayList<Integer[][]> templ2=new ArrayList<Integer[][]>();
 				for(int b=0;b<=a;b++){
 					templ2.add(null);
 				}
 				l2.add(templ2);
 			}
-			for(int a=0;a<5;a++){
-				for(int b=a+1;b<5;b++){
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
 					Integer[][] tempInt=new Integer[data.get(a).size()][data.get(b).size()];
 					for(int c=0;c<length;c++){
 						if(tempInt[appear[c][a]][appear[c][b]]==null){
@@ -98,21 +98,22 @@ public class Proj {
 				}
 			}
 			int len=0;
-			for(int a=0;a<5;a++){
-				for(int b=a+1;b<5;b++){
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
 					for(int c=0;c<l2.get(a).get(b).length;c++){
 						len+=l2.get(a).get(b)[c].length;
 					}
 				}
 			}
+			System.out.println("lenDone");
 			int[] t1=new int[len];
 			int[] t2=new int[len];
 			int[] t1v=new int[len];
 			int[] t2v=new int[len];
 			int[] l2val=new int[len];
 			int head=0;
-			for(int a=0;a<5;a++){
-				for(int b=a+1;b<5;b++){
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
 					for(int c=0;c<l2.get(a).get(b).length;c++){
 						for(int d=0;d<l2.get(a).get(b)[c].length;d++){
 							if(l2.get(a).get(b)[c][d]==null){
@@ -129,29 +130,43 @@ public class Proj {
 					}
 				}
 			}
-			for (int i = len-1; i > 0; --i){
+			System.out.println("headDone");
+			ArrayList<ArrayList<Integer>> struct=new ArrayList<ArrayList<Integer>>();
+			ArrayList<Integer> integ=new ArrayList<Integer>();
+			for(int a=0;a<len;a++){
+				tempIndex=integ.indexOf(l2val[a]);
+				if(tempIndex==-1){
+					integ.add(l2val[a]);
+					ArrayList<Integer> tempStruct=new ArrayList<Integer>();
+					tempStruct.add(a);
+					struct.add(tempStruct);
+				}else{
+					struct.get(tempIndex).add(a);
+				}
+			}
+			for (int i = integ.size()-1; i > 0; --i){
 	            for (int j = 0; j < i; ++j){
-	                if (l2val[j] < l2val[j + 1]){
-	                    int tmp=l2val[j];
-	                    l2val[j]=l2val[j+1];
-	                    l2val[j+1]=tmp;
-	                    tmp=t1[j];
-	                    t1[j]=t1[j+1];
-	                    t1[j+1]=tmp;
-	                    tmp=t2[j];
-	                    t2[j]=t2[j+1];
-	                    t2[j+1]=tmp;
-	                    tmp=t1v[j];
-	                    t1v[j]=t1v[j+1];
-	                    t1v[j+1]=tmp;
-	                    tmp=t2v[j];
-	                    t2v[j]=t2v[j+1];
-	                    t2v[j+1]=tmp;
+	                if (integ.get(j) < integ.get(j+1)){
+	                    Integer tmpInt=integ.get(j);
+	                    integ.set(j, integ.get(j+1));
+	                    integ.set(j+1, tmpInt);
+	                    ArrayList<Integer> tmpIntList=struct.get(j);
+	                    struct.set(j, struct.get(j+1));
+	                    struct.set(j+1, tmpIntList);
 	                }
 	            }
 			}
-			for(int a=0;a<k||l2val[a]==l2val[k-1];a++){
-				writer.println(keys[t1[a]]+":"+data.get(t1[a]).get(t1v[a])+","+keys[t2[a]]+":"+data.get(t2[a]).get(t2v[a])+";"+l2val[a]);
+			System.out.println("sortDone");
+			for(int b=0, a=0, c=0;b<len&&c<k;b++){
+				for(int d=0;d<struct.get(b).size();d++,c++){
+					a=struct.get(b).get(d);
+					if(data.get(t1[a]).get(t1v[a]).length()*data.get(t2[a]).get(t2v[a]).length()==0){
+						c--;
+						continue;
+					}
+					writer.println(keys[t1[a]]+":"+data.get(t1[a]).get(t1v[a])+","+keys[t2[a]]+":"+data.get(t2[a]).get(t2v[a])+";"+l2val[a]);
+				}
+				
 			}
 			writer.close();
 			System.out.println("done");
@@ -159,21 +174,23 @@ public class Proj {
 		if(l==3){
 			ArrayList<ArrayList<ArrayList<Integer[][][]>>> l3;
 			l3 =new ArrayList<ArrayList<ArrayList<Integer[][][]>>>();
-			for(int a=0;a<5;a++){
+			
+			for(int a=0;a<keys.length;a++){
 				ArrayList<ArrayList<Integer[][][]>> templ31=new ArrayList<ArrayList<Integer[][][]>>();
-				for(int b=0;b<5;b++){
+				for(int b=0;b<keys.length;b++){
 					ArrayList<Integer[][][]> templ32=new ArrayList<Integer[][][]>();
-					for(int c=0;c<5;c++){
+					for(int c=0;c<keys.length;c++){
 						templ32.add(null);
 					}
 					templ31.add(templ32);
 				}
 				l3.add(templ31);
 			}
-			for(int a=0;a<5;a++){
-				for(int b=a+1;b<5;b++){
+			
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
 				ArrayList<Integer[][][]> templ32=new ArrayList<Integer[][][]>();
-					for(int c=0;c<5;c++){
+					for(int c=0;c<keys.length;c++){
 						if(c<b+1){
 							templ32.add(null);
 						}else{
@@ -191,9 +208,9 @@ public class Proj {
 				}
 			}
 			int len=0;
-			for(int a=0;a<5;a++){
-				for(int b=a+1;b<5;b++){
-					for(int c=b+1;c<5;c++){
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
+					for(int c=b+1;c<keys.length;c++){
 						for(int d=0;d<l3.get(a).get(b).get(c).length;d++){
 							for(int e=0;e<l3.get(a).get(b).get(c)[d].length;e++){
 								len+=l3.get(a).get(b).get(c)[d][e].length;
@@ -211,9 +228,9 @@ public class Proj {
 			int[] t3v=new int[len];
 			int[] l3val=new int[len];
 			int head=0;
-			for(int a=0;a<5;a++){
-				for(int b=a+1;b<5;b++){
-					for(int c=b+1;c<5;c++){
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
+					for(int c=b+1;c<keys.length;c++){
 						for(int d=0;d<l3.get(a).get(b).get(c).length;d++){
 							for(int f=0;f<l3.get(a).get(b).get(c)[d].length;f++){
 									for(int g=0;g<l3.get(a).get(b).get(c)[d][f].length;g++){
@@ -265,7 +282,11 @@ public class Proj {
 			for(int b=0, a=0, c=0;b<len&&c<k;b++){
 				for(int d=0;d<struct.get(b).size();d++,c++){
 					a=struct.get(b).get(d);
-					writer.println(keys[t1[a]]+":"+data.get(t1[a]).get(t1v[a])+","+keys[t2[a]]+":"+data.get(t2[a]).get(t2v[a])+";"+","+keys[t3[a]]+":"+data.get(t3[a]).get(t3v[a])+";"+l3val[a]);
+					if(data.get(t1[a]).get(t1v[a]).length()*data.get(t2[a]).get(t2v[a]).length()*data.get(t3[a]).get(t3v[a]).length()==0){
+						c--;
+						continue;
+					}
+					writer.println(keys[t1[a]]+":"+data.get(t1[a]).get(t1v[a])+","+keys[t2[a]]+":"+data.get(t2[a]).get(t2v[a])+","+keys[t3[a]]+":"+data.get(t3[a]).get(t3v[a])+";"+l3val[a]);
 				}
 				
 			}
@@ -273,61 +294,106 @@ public class Proj {
 			System.out.println("done");
 		}
 		if(l==4){
-			int[][] l4order={{0, 1, 2, 3}, {0, 1, 2, 4}, {0, 1, 3, 4}, {0, 2, 3, 4}, {1, 2, 3, 4}};
-			ArrayList<Integer[][][][]> l4;
-			l4 =new ArrayList<Integer[][][][]>();
-			for(int a=0;a<5;a++){
-				l4.add(new Integer[data.get(l4order[a][0]).size()][data.get(l4order[a][1]).size()][data.get(l4order[a][2]).size()][data.get(l4order[a][3]).size()]);
-			}
-			
-			for(int a=0;a<5;a++){
-				for(int b=0;b<length;b++){
-					if(l4.get(a)[appear[b][l4order[a][0]]][appear[b][l4order[a][1]]][appear[b][l4order[a][2]]][appear[b][l4order[a][3]]]==null){
-						l4.get(a)[appear[b][l4order[a][0]]][appear[b][l4order[a][1]]][appear[b][l4order[a][2]]][appear[b][l4order[a][3]]]=0;
+			ArrayList<ArrayList<ArrayList<ArrayList<Integer[][][][]>>>> l4;
+			l4 =new ArrayList<ArrayList<ArrayList<ArrayList<Integer[][][][]>>>>();
+			for(int a=0;a<keys.length;a++){
+				ArrayList<ArrayList<ArrayList<Integer[][][][]>>> templ41=new ArrayList<ArrayList<ArrayList<Integer[][][][]>>>();
+				for(int b=0;b<keys.length;b++){
+					ArrayList<ArrayList<Integer[][][][]>> templ42=new ArrayList<ArrayList<Integer[][][][]>>();
+					for(int c=0;c<keys.length;c++){
+						ArrayList<Integer[][][][]> templ43=new ArrayList<Integer[][][][]>();
+						for(int d=0;d<keys.length;d++){
+							templ43.add(null);
+						}
+						templ42.add(templ43);
 					}
-					l4.get(a)[appear[b][l4order[a][0]]][appear[b][l4order[a][1]]][appear[b][l4order[a][2]]][appear[b][l4order[a][3]]]++;
+					templ41.add(templ42);
+				}
+				l4.add(templ41);
+			}
+
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
+					for(int c=b+1;c<keys.length;c++){
+						ArrayList<Integer[][][][]> templ43=new ArrayList<Integer[][][][]>();
+						for(int d=0;d<keys.length;d++){
+							if(d<c+1){
+								templ43.add(null);
+							}else{
+								Integer[][][][] tempInt4=new Integer[data.get(a).size()][data.get(b).size()][data.get(c).size()][data.get(d).size()];
+								for(int e=0;e<length;e++){
+									if(tempInt4[appear[e][a]][appear[e][b]][appear[e][c]][appear[e][d]]==null){
+										tempInt4[appear[e][a]][appear[e][b]][appear[e][c]][appear[e][d]]=0;
+									}
+									tempInt4[appear[e][a]][appear[e][b]][appear[e][c]][appear[e][d]]++;
+								}
+								templ43.add(tempInt4);
+							}
+							l4.get(a).get(b).set(c, templ43);
+						}
+					}
 				}
 			}
-				
+			
 			int len=0;
-			for(int a=0;a<5;a++){
-				for(int b=0;b<l4.get(a).length;b++){
-					for(int c=0;c<l4.get(a)[b].length;c++){
-						for(int d=0;d<l4.get(a)[b][c].length;d++){
-							len+=l4.get(a)[b][c][d].length;
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
+					for(int c=b+1;c<keys.length;c++){
+						for(int d=c+1;d<keys.length;d++){
+							for(int e=0;e<l4.get(a).get(b).get(c).get(d).length;e++){
+								for(int f=0;f<l4.get(a).get(b).get(c).get(d)[e].length;f++){
+									for(int g=0;g<l4.get(a).get(b).get(c).get(d)[e][f].length;g++){
+										len+=l4.get(a).get(b).get(c).get(d)[e][f][g].length;
+									}
+								}
+							}
 						}
 					}
 				}
 			}
 			System.out.println("lenDone");
-			int[] elorder=new int[len];
+			int[] t1=new int[len];
+			int[] t2=new int[len];
+			int[] t3=new int[len];
+			int[] t4=new int[len];
 			int[] t1v=new int[len];
 			int[] t2v=new int[len];
 			int[] t3v=new int[len];
 			int[] t4v=new int[len];
 			int[] l4val=new int[len];
 			int head=0;
-			for(int a=0;a<5;a++){
-				for(int b=0;b<l4.get(a).length;b++){
-					for(int c=0;c<l4.get(a)[b].length;c++){
-						for(int d=0;d<l4.get(a)[b][c].length;d++){
-							for(int e=0;e<l4.get(a)[b][c][d].length;e++){
-								elorder[head]=a;
-								t1v[head]=b;
-								t2v[head]=c;
-								t3v[head]=d;
-								t4v[head]=e;
-								if(l4.get(a)[b][c][d][e]==null){
-									l4val[head]=0;
-								}else{
-									l4val[head]=l4.get(a)[b][c][d][e];
+			
+			for(int a=0;a<keys.length;a++){
+				for(int b=a+1;b<keys.length;b++){
+					for(int c=b+1;c<keys.length;c++){
+						for(int d=c+1;d<keys.length;d++){
+							for(int e=0;e<l4.get(a).get(b).get(c).get(d).length;e++){
+								for(int f=0;f<l4.get(a).get(b).get(c).get(d)[e].length;f++){
+									for(int g=0;g<l4.get(a).get(b).get(c).get(d)[e][f].length;g++){
+										for(int h=0;h<l4.get(a).get(b).get(c).get(d)[e][f][g].length;h++){
+											if(l4.get(a).get(b).get(c).get(d)[e][f][g][h]==null){
+												l4val[head]=0;
+											}else{
+												l4val[head]=l4.get(a).get(b).get(c).get(d)[e][f][g][h];
+											}
+											t1[head]=a;
+											t2[head]=b;
+											t3[head]=c;
+											t4[head]=d;
+											t1v[head]=e;
+											t2v[head]=f;
+											t3v[head]=g;
+											t4v[head]=h;
+											head++;
+										}
+									}
 								}
-								head++;
 							}
 						}
 					}
 				}
 			}
+			
 			System.out.println("headDone");
 			ArrayList<ArrayList<Integer>> struct=new ArrayList<ArrayList<Integer>>();
 			ArrayList<Integer> integ=new ArrayList<Integer>();
@@ -358,7 +424,11 @@ public class Proj {
 			for(int b=0, a=0, c=0;b<len&&c<k;b++){
 				for(int d=0;d<struct.get(b).size();d++,c++){
 					a=struct.get(b).get(d);
-					writer.println(keys[l4order[elorder[a]][0]]+":"+data.get(l4order[elorder[a]][0]).get(t1v[a])+","+keys[l4order[elorder[a]][1]]+":"+data.get(l4order[elorder[a]][1]).get(t2v[a])+","+keys[l4order[elorder[a]][2]]+":"+data.get(l4order[elorder[a]][2]).get(t3v[a])+","+keys[l4order[elorder[a]][3]]+":"+data.get(l4order[elorder[a]][3]).get(t4v[a])+";"+l4val[a]);
+					if(data.get(t1[a]).get(t1v[a]).length()*data.get(t2[a]).get(t2v[a]).length()*data.get(t3[a]).get(t3v[a]).length()*data.get(t4[a]).get(t4v[a]).length()==0){
+						c--;
+						continue;
+					}
+					writer.println(keys[t1[a]]+":"+data.get(t1[a]).get(t1v[a])+","+keys[t2[a]]+":"+data.get(t2[a]).get(t2v[a])+","+keys[t3[a]]+":"+data.get(t3[a]).get(t3v[a])+","+keys[t4[a]]+":"+data.get(t4[a]).get(t4v[a])+";"+l4val[a]);
 				}
 				
 			}
